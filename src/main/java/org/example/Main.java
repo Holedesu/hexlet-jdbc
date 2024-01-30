649a61f5a9b31095386b78ec24139c83fc7dc234
Holedesu <88774183+Holedesu@users.noreply.github.com>
2024-01-30 20:41:57 +0300

    Add operator try with resources to the code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,56 @@
 package org.example;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws SQLException {
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        var conn = DriverManager.getConnection("jdbc:h2:mem:hexlet");
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        var sql = "CREATE TABLE films"
+                + "(id BIGINT PRIMARY KEY AUTO_INCREMENT, title VARCHAR(255), release_year INT, duration INT)";
+
+        var statement = conn.createStatement();
+        statement.execute(sql);
+        statement.close();
+
+        // BEGIN (write your solution here)
+        var sql2 = "INSERT INTO films (title, release_year, duration) VALUES ('Godfather', '1972', '175'), ('The Green Mile', '1999', '189')";
+        var statement2 = conn.createStatement();
+        statement2.executeUpdate(sql2);
+        statement2.close();
+
+        var sql3 = "SELECT * FROM films";
+        var statement3 = conn.createStatement();
+
+        var resultSet = statement3.executeQuery(sql3);
+
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("title") + " "
+                    + resultSet.getString("release_year") + " "
+                    + resultSet.getString("duration"));
+
         }
+        statement3.close();
+
+//        var sql4 = "INSERT INTO films (title, release_year, duration) VALUES ('The Green Mile', '1999', '189')";
+//        var statement4 = conn.createStatement();
+//        statement4.executeUpdate(sql4);
+//        statement3.close();
+//
+//        var resultSet2 = statement3.executeQuery(sql4);
+//        while (resultSet2.next()) {
+//            System.out.println(resultSet2.getString("title") + " "
+//                    + resultSet2.getString("release_year") + " "
+//                    + resultSet2.getString("duration"));
+//
+//        }
+        statement3.close();
+        // END
+
+        conn.close();
     }
 }
