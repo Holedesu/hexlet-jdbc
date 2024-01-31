f344ec22b4228491b95aaf344c325cf9bed2206b
Holedesu <88774183+Holedesu@users.noreply.github.com>
2024-01-31 16:34:38 +0300

    Added to data base new user and delete old one

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -15,10 +15,26 @@ public class App{
                 statement.execute(sql);
             }
 
-            var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
-            try (var statement2 = conn.createStatement()) {
-                statement2.executeUpdate(sql2);
+            var sql2 = "INSERT INTO users (username, phone) VALUES (?, ?)";
+            try (var preparedStatement = conn.prepareStatement(sql2)) {
+                preparedStatement.setString(1, "Tommy");
+                preparedStatement.setString(2, "123456789");
+                preparedStatement.executeUpdate();
+
+                preparedStatement.setString(1, "Jimmy");
+                preparedStatement.setString(2, "123383489");
+                preparedStatement.executeUpdate();
+
+
             }
+
+            var sql23  = "DELETE FROM users WHERE username=?";
+            try (var preparedStatement = conn.prepareStatement(sql23)){
+                preparedStatement.setString(1, "Jimmy");
+                preparedStatement.executeUpdate();
+            }
+
+
 
             var sql3 = "SELECT * FROM users";
             try (var statement3 = conn.createStatement()) {
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,50 +7,30 @@ import java.sql.SQLException;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws SQLException {
+        // Соединение с базой данных тоже нужно отслеживать
+        try (var conn = DriverManager.getConnection("jdbc:h2:mem:cars")) {
 
-        var conn = DriverManager.getConnection("jdbc:h2:mem:hexlet");
+            var sql = "CREATE TABLE cars (id BIGINT PRIMARY KEY AUTO_INCREMENT, brand VARCHAR(255), model VARCHAR(255))";
+            try (var statement = conn.createStatement()) {
+                statement.execute(sql);
+            }
 
-        var sql = "CREATE TABLE films"
-                + "(id BIGINT PRIMARY KEY AUTO_INCREMENT, title VARCHAR(255), release_year INT, duration INT)";
+            var sql2 = "INSERT INTO cars (brand, model) VALUES ('kia', 'sorento'), ('bmw', 'x5'), ('audi', 'q7')";
+            try (var statement2 = conn.createStatement()) {
+                statement2.executeUpdate(sql2);
+            }
 
-        var statement = conn.createStatement();
-        statement.execute(sql);
-        statement.close();
 
-        // BEGIN (write your solution here)
-        var sql2 = "INSERT INTO films (title, release_year, duration) VALUES ('Godfather', '1972', '175'), ('The Green Mile', '1999', '189')";
-        var statement2 = conn.createStatement();
-        statement2.executeUpdate(sql2);
-        statement2.close();
 
-        var sql3 = "SELECT * FROM films";
-        var statement3 = conn.createStatement();
+            var sql3 = "SELECT * FROM cars ORDER BY brand ASC";
+            try (var statement3 = conn.createStatement()) {
+                var resultSet = statement3.executeQuery(sql3);
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("brand") + " " +
+                            resultSet.getString("model"));
 
-        var resultSet = statement3.executeQuery(sql3);
-
-        while (resultSet.next()) {
-            System.out.println(resultSet.getString("title") + " "
-                    + resultSet.getString("release_year") + " "
-                    + resultSet.getString("duration"));
-
+                }
+            }
         }
-        statement3.close();
-
-//        var sql4 = "INSERT INTO films (title, release_year, duration) VALUES ('The Green Mile', '1999', '189')";
-//        var statement4 = conn.createStatement();
-//        statement4.executeUpdate(sql4);
-//        statement3.close();
-//
-//        var resultSet2 = statement3.executeQuery(sql4);
-//        while (resultSet2.next()) {
-//            System.out.println(resultSet2.getString("title") + " "
-//                    + resultSet2.getString("release_year") + " "
-//                    + resultSet2.getString("duration"));
-//
-//        }
-        statement3.close();
-        // END
-
-        conn.close();
     }
 }
